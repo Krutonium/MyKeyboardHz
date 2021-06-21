6f6c8504cbd7bd38429e4c4782b7ff27df821050
pfckr <pfckr@DESKTOP-FM84LBE>
2021-06-21 00:07:11 -0400

    Add project files.

---
+++ MyKeyboardHz/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace MyKeyboardHz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lblScanRate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press Any Non-Enter Key to Measure";
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(12, 39);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(59, 17);
+            this.lblResults.TabIndex = 2;
+            this.lblResults.Text = "Results:";
+            // 
+            // lblScanRate
+            // 
+            this.lblScanRate.AutoSize = true;
+            this.lblScanRate.Location = new System.Drawing.Point(330, 9);
+            this.lblScanRate.Name = "lblScanRate";
+            this.lblScanRate.Size = new System.Drawing.Size(144, 17);
+            this.lblScanRate.TabIndex = 3;
+            this.lblScanRate.Text = "Estimated Scan Rate:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 450);
+            this.Controls.Add(this.lblScanRate);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "My Keyboard Hz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label lblScanRate;
+    }
+}
+
---
+++ MyKeyboardHz/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyKeyboardHz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static DateTime startTime = DateTime.Now;
+        private static DateTime endTime = DateTime.Now;
+        private static int shortestPress = int.MaxValue;
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter) { return; }
+            startTime = DateTime.Now;
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) { return; }
+            endTime = DateTime.Now;
+            var time = endTime - startTime;
+            lblResults.Text += Environment.NewLine + time.Milliseconds.ToString();
+            if(time.Milliseconds < shortestPress)
+            {
+                shortestPress = time.Milliseconds;
+            }
+            lblScanRate.Text = "Estimated Scan Rate: " + Environment.NewLine + (1000f / shortestPress) + "Hz";
+        }
+    }
+}
